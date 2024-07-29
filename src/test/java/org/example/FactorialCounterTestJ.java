df785288f29f8f695908efda69bf38fbe2b3e404
HamaniEkaterina <kat.vee.cs@gmail.com>
2024-07-29 16:43:14 +0300

    Первый коммит

--- src/test/java/org/example/FactorialCounterTestJ.java
+++ src/test/java/org/example/FactorialCounterTestJ.java
@@ -1,24 +1,15 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class FactorialCounterTest {
-
-    private FactorialCounter factorialCounter;
-
-    @BeforeEach
-    void setUp(){
-        factorialCounter = new FactorialCounter();
-    }
+class FactorialCounterTestJ {
 
     @Test
     @DisplayName("Factorial of zero")
     void testFactorialWithZero() {
+        FactorialCounter factorialCounter = new FactorialCounter();
         int factorial = FactorialCounter.factorial(0);
         Assertions.assertEquals(1, factorial);
     }
@@ -26,18 +17,21 @@ class FactorialCounterTest {
     @Test
     @DisplayName("Factorial with positive number")
     void testFactorialWithPositiveNumber() {
+        FactorialCounter factorialCounter = new FactorialCounter();
         int factorial = FactorialCounter.factorial(2);
         Assertions.assertEquals(2, factorial);
     }
 
     @Test
     @DisplayName("Factorial with negative number")
-    void testFactorialWithNegativeNumber(){
-        try{
+    void testFactorialWithNegativeNumber() {
+        FactorialCounter factorialCounter = new FactorialCounter();
+        try {
             int factorial = FactorialCounter.factorial(-1);
             Assertions.fail("Предполагается IllegalArgumentException для отрицательного числа");
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
 
         }
 
-}}
+    }
+}
