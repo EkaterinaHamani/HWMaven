861c45e9974ab4a07809088bbfb95d3c7056c325
HamaniEkaterina <kat.vee.cs@gmail.com>
2024-08-04 16:13:46 +0300

    Первый коммит

---
+++ src/test/java/FactorialCounterTestJ.java
@@ -0,0 +1,31 @@
+import org.example.FactorialCounter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class FactorialCounterTestJ {
+
+    @Test
+    @DisplayName("Factorial of zero")
+    void testFactorialWithZero() {
+        int factorial = FactorialCounter.factorial(0);
+        Assertions.assertEquals(1, factorial);
+    }
+
+    @Test
+    @DisplayName("Factorial with positive number")
+    void testFactorialWithPositiveNumber() {
+        int factorial = FactorialCounter.factorial(1);
+        Assertions.assertEquals(1, factorial);
+    }
+
+    @Test
+    @DisplayName("Factorial with negative number")
+    void testFactorialWithNegativeNumber() {
+        Throwable exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            FactorialCounter.factorial(-1);
+        });
+    }
+}
+
